25a28ec99258395298932c61cac8ac8ca2ba0f5b
honghan <honghan.wu@gmail.com>
2020-06-28 11:35:04 +0100

    models updated

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
 import pandas as pd
 import random
+from enum import Enum
+import logging
+import math
 
 
 class PredictionModel(object):
@@ -97,11 +100,22 @@ class DecisionTree(PredictionModel):
         self._params = {}
         self._root = None
         self._nodes = {}
+        self._prob_mode = DTProbModel.m_estimation
+        self._event_rate = 0.5
         super(DecisionTree, self).__init__(model_data)
+
+    @property
+    def prob_mode(self):
+        return self._prob_mode
+
+    @prob_mode.setter
+    def prob_model(self, v):
+        self._prob_mode = v
 
     def load_model_detail(self, model_detail):
         if 'nodes' not in model_detail:
             raise Exception('decision tree model does not find nodes')
+        self._event_rate = model_detail['event_rate']
         nodes = model_detail['nodes']
         for n in nodes:
             node = TreeNode(n)
@@ -120,11 +134,46 @@ class DecisionTree(PredictionModel):
                 ret = n.to(r)
                 if 'outcome' in ret:
                     # print('outcome at {0}, {1}'.format(n.op, n.variable), idx, ret)
-                    probs.append(ret['outcome'])
+                    prob = ret['outcome']
+                    if 'support' in ret:
+                        prob = self.cal_prob(ret['outcome'], ret['support'])
+                    probs.append(prob)
                     break
                 else:
                     n = self._nodes[ret['id']]
         return probs
+
+    def cal_prob(self, outcome, support):
+        s_t = support['T']
+        s_f = support['F']
+        if outcome == 0:
+            s_t = support['F']
+            s_f = support['T']
+        if self.prob_mode == DTProbModel.maximum_likelihood:
+            return DecisionTree.maximum_likelihood(s_t, s_f)
+        elif self.prob_mode == DTProbModel.laplace_estimate:
+            return DecisionTree.laplace_estimate(s_t, s_f)
+        elif self.prob_mode == DTProbModel.m_estimation:
+            return DecisionTree.m_estimation(s_t, s_f, self._event_rate)
+
+    @staticmethod
+    def maximum_likelihood(case_support, control_support):
+        return 1.0 * case_support / (case_support + control_support)
+
+    @staticmethod
+    def laplace_estimate(case_support, control_support):
+        return 1.0 * (case_support + 1)/ (case_support + control_support + 2)
+
+    @staticmethod
+    def m_estimation(case_support, control_support, prior_prob):
+        m = 10 / prior_prob
+        return 1.0 * (case_support + m * prior_prob)/ (case_support + control_support + m)
+
+
+class DTProbModel(Enum):
+    maximum_likelihood = 1,
+    laplace_estimate = 2,
+    m_estimation = 3
 
 
 class TreeNode(object):
@@ -181,19 +230,23 @@ class ScoringModel(PredictionModel):
     def __init__(self, model_data):
         self._params = {}
         self._max_score = 0
+        self._score_to_prob = {}
         super(ScoringModel, self).__init__(model_data)
 
     def load_model_detail(self, model_detail):
-        for v in model_detail:
-            self._params[v] = model_detail[v]
-            self._max_score += model_detail[v]['yes'] \
-                if model_detail[v]['yes'] > model_detail[v]['no'] else model_detail[v]['no']
+        for v in model_detail['variables']:
+            self._params[v] = model_detail['variables'][v]
+            self._max_score += model_detail['variables'][v]['yes'] \
+                if model_detail['variables'][v]['yes'] > model_detail['variables'][v]['no'] else model_detail['variables'][v]['no']
+        for t in model_detail['score_probs']:
+            self._score_to_prob[str(t[0])] = t[1]
 
     @property
     def max_score(self):
         return self._max_score
 
     def predict_prob(self, x):
+        from math import exp
         self.check_x(x)
         probs = []
         for idx, r in x.iterrows():
@@ -203,8 +256,17 @@ class ScoringModel(PredictionModel):
                     raise Exception('variable [{0}] not found in [{1}]'.format(p, r))
                 op = self._params[p]
                 score += op['yes'] if ModelUtil.binary_operator(op, r[p]) else op['no']
-            probs.append(1.0 * score / self.max_score)
+            probs.append(self.get_prob(score))
+            # probs.append(1 / (1 + exp(-score)))
         return probs
+
+    def get_prob(self, score):
+        s = str(score)
+        if s not in self._score_to_prob:
+            logging.warn('score {0} not in score_to_prob data'.format(s))
+            return 1.0 * score / self.max_score
+        else:
+            return self._score_to_prob[str(score)]
 
 
 class NomogramModel(PredictionModel):
@@ -238,9 +300,18 @@ class NomogramModel(PredictionModel):
             val = r[v]
             cal = self._params[v]
             if cal['type'] == 'discrete':
+                matched = False
+                last_point = 0
                 for t in cal['map']:
                     if t['range'][0] <= val < t['range'][1]:
                         points += t['point']
+                        matched = True
+                        break
+                    else:
+                        last_point = t['point']
+                if not matched:
+                    logging.info('{0} value {1} not matched, using nearest range point'.format(v, val))
+                    points += last_point
             else:
                 point_range = t['point']
                 val_range = t['variable']
@@ -270,6 +341,79 @@ class NomogramModel(PredictionModel):
                 return 1
 
 
+class NOCOS(PredictionModel):
+    """
+    a function reimplemented from https://cbmi.northwell.edu/nocos/
+    """
+    def __init__(self, model_data):
+        self._detail = {}
+        super(NOCOS, self).__init__(model_data)
+
+    def predict_prob(self, x):
+        self.check_x(x)
+        probs = []
+        for idx, r in x.iterrows():
+            probs.append(1 - NOCOS.covid19SurvivalProbabilityFormula_v4(r, self._detail['calculationData']))
+        return probs
+
+    def load_model_detail(self, model_detail):
+        self._detail = model_detail
+
+    @staticmethod
+    def covid19SurvivalProbabilityFormula_v4(factorInput, calculationData):
+        predictorData = calculationData['predictorData']
+        bayesData = calculationData['bayesData']
+
+        x = 0.0
+        for pd in predictorData:
+            if pd['id'] in factorInput:
+                factorValue = float(factorInput[pd['id']])
+            else:
+                factorValue = 0.0
+            ## ignore validate not convertable values as an error would be raised anyway
+            # if factorValue is None:
+            #     factorValue = 0.0
+
+            sigma = 1.0 * pd['sigma']
+            mu = 1.0 * pd['mu']
+            coefficient = 1.0 * pd['coefficient']
+            x = x + coefficient * ((factorValue - mu) / sigma)
+
+        LPos = 0.0
+        posteriorData = calculationData['paretoData']['posteriorPos']
+        if x < posteriorData['threshold1']:
+            LPos = NOCOS.lowerParetoTail(posteriorData['p1'], posteriorData['sigma1'], posteriorData['k1'], posteriorData['threshold1'], x)
+        elif x > posteriorData['threshold2']:
+            LPos = NOCOS.upperParetoTail(posteriorData['p2'], posteriorData['sigma2'], posteriorData['k2'], posteriorData['threshold2'], x)
+        else:
+            LPos = NOCOS.polynomial(posteriorData['coefficients'], x)
+
+        LNeg = 0.0
+        posteriorData = calculationData['paretoData']['posteriorNeg']
+        if x < posteriorData['threshold1']:
+            LNeg = NOCOS.lowerParetoTail(posteriorData['p1'], posteriorData['sigma1'], posteriorData['k1'], posteriorData['threshold1'], x)
+        elif x > posteriorData['threshold2']:
+            LNeg = NOCOS.upperParetoTail(posteriorData['p2'], posteriorData['sigma2'], posteriorData['k2'], posteriorData['threshold2'], x)
+        else:
+            LNeg = NOCOS.polynomial(posteriorData['coefficients'], x)
+        return LPos * calculationData['paretoData']['priorPos'] / (LPos * calculationData['paretoData']['priorPos'] + LNeg * calculationData['paretoData']['priorNeg'])
+
+    @staticmethod
+    def lowerParetoTail(p, sigma, k, threshold, randomVar):
+        return p * (1.0 / sigma) * math.pow((1.0 + k * (threshold - randomVar) / sigma), (-1.0 - (1.0 / k)))
+
+    @staticmethod
+    def upperParetoTail(p, sigma, k, threshold, randomVar):
+        return (1.0 - p) * (1 / sigma) * math.pow((1.0 + k * (randomVar - threshold) / sigma), (-1.0 - (1.0 / k)))
+
+    @staticmethod
+    def polynomial(coefficients, randomVar):
+        result = 0.0
+        for i in range(len(coefficients)):
+            result += coefficients[i] * (randomVar ** i)
+        return result
+
+
 class Imputator(object):
     """
     abstract imputation class
@@ -286,6 +430,7 @@ class DistributionImputator(Imputator):
     use median and iqr to impute data
     """
     def __init__(self, dist):
+        super().__init__()
         self._dist = dist
 
     def impute(self, x, variables):
